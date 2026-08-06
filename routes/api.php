1cb00779b4af749881bd0c47edef258d7ea3a6ac
estevaolocks <estevao.locks11@gmail.com>
2026-08-06 01:12:50 -0300

    iniciando api com rotas em controller - usuarios

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/usuarios', UsuarioController::class);
